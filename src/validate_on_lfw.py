ca503d08bb02461ca98b0a1ee869d25e8fa07766
darwin-xu <xu.dai.wen@126.com>
2018-04-09 23:43:41 +0800

    DEV: print more info

--- src/validate_on_lfw.py
+++ src/validate_on_lfw.py
@@ -96,16 +96,35 @@ def main(args):
             nrof_batches = int(math.ceil(1.0*nrof_images / batch_size))
             emb_array = np.zeros((nrof_images, embedding_size))
             for i in range(nrof_batches):
+                print('Compute batch:', i, '/', nrof_batches, end='\r')
                 start_index = i*batch_size
                 end_index = min((i+1)*batch_size, nrof_images)
                 paths_batch = paths[start_index:end_index]
                 images = facenet.load_data(paths_batch, False, False, image_size)
                 feed_dict = { images_placeholder:images, phase_train_placeholder:False }
                 emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
+            print()
+            print('--- Evaluate ---')
 
             tpr, fpr, accuracy, threshold, val, val_std, far = lfw.evaluate(emb_array,
                 actual_issame, nrof_folds=args.lfw_nrof_folds)
 
+            print()
+            print('--- Verify using Euclidian distance ---')
+            print('Accuracy: %1.3f+-%1.3f' % (np.mean(accuracy), np.std(accuracy)))
+            print('Best threshold: %1.3f' % threshold)
+            print('Validation rate: %2.5f+-%2.5f @ FAR=%2.5f' % (val, val_std, far))
+
+            auc = metrics.auc(fpr, tpr)
+            print('Area Under Curve (AUC): %1.3f' % auc)
+            eer = brentq(lambda x: 1. - x - interpolate.interp1d(fpr, tpr)(x), 0., 1.)
+            print('Equal Error Rate (EER): %1.3f' % eer)
+
+            tpr, fpr, accuracy, threshold, val, val_std, far = lfw.evaluate(emb_array,
+                actual_issame, nrof_folds=args.lfw_nrof_folds, distance_metric=1)
+
+            print()
+            print("--- Verify using Cosine Similarity ---")
             print('Accuracy: %1.3f+-%1.3f' % (np.mean(accuracy), np.std(accuracy)))
             print('Best threshold: %1.3f' % threshold)
             print('Validation rate: %2.5f+-%2.5f @ FAR=%2.5f' % (val, val_std, far))
